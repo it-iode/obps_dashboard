708c946a509c44b4760a8594ccd79957950d22de
a.lambert@unesco.org <r3d!zeb?raGi>
2023-08-24 14:53:06 +0200

    replace url by re_path

--- obps_dashboard/urls.py
+++ obps_dashboard/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import re_path, include
     2. Add a URL to urlpatterns:  re_path(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import re_path
+from django.urls import re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
