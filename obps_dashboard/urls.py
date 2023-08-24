ca6ff82a9358e2c3f294dc5fa579faa1224c5222
a.lambert@unesco.org <r3d!zeb?raGi>
2023-08-24 14:51:40 +0200

    replace url by re_path

--- obps_dashboard/urls.py
+++ obps_dashboard/urls.py
@@ -5,32 +5,32 @@ The `urlpatterns` list routes URLs to views. For more information please see:
 Examples:
 Function views
     1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+    2. Add a URL to urlpatterns:  re_path(r'^$', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+    2. Add a URL to urlpatterns:  re_path(r'^$', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+    1. Import the include() function: from django.conf.urls import re_path, include
+    2. Add a URL to urlpatterns:  re_path(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from obps_dashboard.apps import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^$', views.start, name='home'),
-    url(r'^dspacemetrics/$', views.dspace_metrics, name='dspacemetrics'),
-    url(r'^obpsrt/$', views.obps_realtime, name='obpsrt'),
-    url(r'^obpshistory/$', views.obps_history, name='obpshistory'),
-    url(r'^obpshistory-mainlanding/$', views.obps_history_mainlanding, name='obpshistory_mainlanding'),
-    url(r'^conferences/$', views.conferences, name='conferences'),
-    url(r'^publications/$', views.publications, name='publications'),
-    url(r'^newsletter/$', views.newsletter, name='newsletter'),
-    url(r'^community_engagement/$', views.community_engagement, name='community_engagement'),
-    url(r'^obps_workshops/$', views.obps_workshops, name='obps_workshops'),
+    re_path(r'^admin/', admin.site.urls),
+    re_path(r'^$', views.start, name='home'),
+    re_path(r'^dspacemetrics/$', views.dspace_metrics, name='dspacemetrics'),
+    re_path(r'^obpsrt/$', views.obps_realtime, name='obpsrt'),
+    re_path(r'^obpshistory/$', views.obps_history, name='obpshistory'),
+    re_path(r'^obpshistory-mainlanding/$', views.obps_history_mainlanding, name='obpshistory_mainlanding'),
+    re_path(r'^conferences/$', views.conferences, name='conferences'),
+    re_path(r'^publications/$', views.publications, name='publications'),
+    re_path(r'^newsletter/$', views.newsletter, name='newsletter'),
+    re_path(r'^community_engagement/$', views.community_engagement, name='community_engagement'),
+    re_path(r'^obps_workshops/$', views.obps_workshops, name='obps_workshops'),
 ]
 
 if settings.DEBUG:
